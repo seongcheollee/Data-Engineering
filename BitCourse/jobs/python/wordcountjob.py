96b722b4f67e7b1e7412b147c667cc31fcf1e9f7
seongcheollee <jhk6520736@gmail.com>
2024-02-20 20:38:44 +0900

    airflow - spark first commit

---
+++ BitCourse/jobs/python/wordcountjob.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("PythonWordCount")\
+    .config("spark.jars", "/opt/spark/jars/postgresql-42.2.5.jar") \
+    .getOrCreate()
+
+text = "Hello Spark Hello Python Hello Airflow Hello Docker and Hello Yusuf"
+
+words = spark.sparkContext.parallelize(text.split(" "))
+
+wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+for wc in wordCounts.collect():
+    print(wc[0], wc[1])
+print('test')
+spark.stop()
